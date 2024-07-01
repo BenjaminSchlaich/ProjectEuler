a637a6f78ca04cec9d2fa59f855acf02f4dd6799
Benjamin Schlaich <benjamin.schlaich@icloud.com>
2024-07-01 20:12:29 +0200

    cleanup and speed measurement for problem 74.

--- Project Euler/Problem74/problem74.cpp
+++ Project Euler/Problem74/problem74.cpp
@@ -1,14 +1,11 @@
 
 #include <iostream>
 #include <vector>
-#include <list>
-#include <unordered_map>
-#include <unordered_set>
-#include <iomanip>
+#include <chrono>
 
 using namespace std;
 
-int factorial[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+const int factorial[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
 
 int iterate(int n)
 {
@@ -25,65 +22,45 @@ int iterate(int n)
     return sum;
 }
 
-struct t
-{
-    int next;       // next element in the sequence
-    int distance;   // distance from repetition (including cycle length)
-    int loopSize;   // <= distance. how large is the loop?
-};
-
 struct map
 {
-    // unordered_map<int, t> m;
-    vector<t> v;
+    vector<pair<int, int>> v;       // first int: next integer in sequence. second int: length of non-repeating elements
 
     map(size_t N)
     {
-        // m = unordered_map<int, t>();
-        v = vector<t>(N + 1);
+        v = vector<pair<int, int>>(N + 1);
     }
 
     bool isKnown(int i)
     {
-        // return m.contains(i);
-
         if(i >= v.size())
         {
-            v.resize(i+1, {-1, -1, -1});
+            v.resize(i+1, {-1, -1});
             return false;
         }
         else
-            return v.at(i).next > 0;
+            return v.at(i).first > 0;
     }
 
     bool isComplete(int i)
     {
-        // return isKnown(i) && m.at(i).distance > 0;
-        return v.at(i).distance > 0;
+        return v.at(i).second > 0;
     }
 
     void setNext(int i0, int i1)
     {
-        // m.insert({i0, {i1, -1, -1}});
-        v.at(i0) = {i1, -1, -1};
+        v.at(i0) = {i1, -1};
     }
 
     int getNext(int i)
     {
-        // return m.at(i).next;
-        return v.at(i).next;
+        return v.at(i).first;
     }
 
     int &getDistance(int i)
     {
         // return m.at(i).distance;
-        return v.at(i).distance;
-    }
-
-    int &getLoopSize(int i)
-    {
-        // return m.at(i).loopSize;
-        return v.at(i).loopSize;
+        return v.at(i).second;
     }
 };
 
@@ -110,7 +87,6 @@ void find(int n, map &m)
         do
         {
             m.getDistance(nn) = circleLength;
-            m.getLoopSize(nn) = circleLength;
             nn = m.getNext(nn);
         } while(nn != next);
 
@@ -120,13 +96,11 @@ void find(int n, map &m)
         while(d > circleLength)
         {
             m.getDistance(nn) = d--;
-            m.getLoopSize(nn) = circleLength;
             nn = m.getNext(nn);
         }
     }
     else
     {
-        circleLength = m.getLoopSize(next);
         d = -(d + 1) + m.getDistance(next);
 
         nn = n;
@@ -134,7 +108,6 @@ void find(int n, map &m)
         while(nn != next)
         {
             m.getDistance(nn) = d--;
-            m.getLoopSize(nn) = circleLength;
             nn = m.getNext(nn);
         }
     }
@@ -159,49 +132,15 @@ void solve()
     cout << "solution: " << count << endl;
 }
 
-void test()
-{
-    const int N = 999999;
-
-    map m = map(N);
-
-    size_t maxCount = 0;
-    int maxN = 0;
-
-    for(int n=0; n<=N; n++)
-    {
-        find(n, m);
-
-        if(m.getDistance(n) > maxCount)
-        {
-            maxCount = m.getDistance(n);
-
-            maxN = n;
-        }
-    }
-
-    cout << "Maximum chain length: " << maxCount << endl;
-    cout << "Maximum starting element: " << maxN << endl;
-    cout << "Sequence:" << endl;
-
-    int n = maxN;
-
-    unordered_set<int> S;
-
-    int i = 1;
-
-    while(!S.contains(n))
-    {
-        S.insert(n);
-        cout << setw(3) << (i++) << ": " << n << endl;
-        n = m.getNext(n);
-    }
-}
-
 int main()
 {
-    
+    auto t0 = chrono::high_resolution_clock::now();
+
     solve();
+
+    auto t1 = chrono::high_resolution_clock::now();
+
+    cout << "Solving the problem took " << chrono::duration_cast<chrono::milliseconds>(t1 - t0) << " milliseconds." << endl;
 
     return 0;
 }
