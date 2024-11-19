c3423f64b8bd5e66efefa19cdaa6c9ea27456dba
Benjamin Schlaich <benjamin.schlaich@icloud.com>
2024-11-19 01:37:56 +0100

    ahh not working yet.

--- Problem327/solve.cpp
+++ Problem327/solve.cpp
@@ -29,36 +29,16 @@ int carryOne(int C, int K)
 {
     if(C <= 2 && K >= 1)
         throw logic_error("carryOne called with impossible arguments: C=" + to_string(C) + " K=" + to_string(K));
-
-    int res = 0;
-
-    while(K > 0)
-    {
-        if(K < C)
-            return res + K + 1;
-        else
-        {
-            K -= C - 2;
-            res += C;
-        }
-    }
-
-    return res;
+    else if(K < C)
+        return K + 1;
+    else
+        return K / (C - 2) * C + (K % (C - 2)) + 1;
 }
 
 int solve(int C, int R, int K)
 {
     if(R == 1)
         return carryOne(C, K);
-    
-    int min = numeric_limits<int>::max();
-
-    for(int r=1; r<R; r++)
-    {
-        int back = solve(C, R - r, K);
-        int whole = solve(C, r, back);
-        min = MIN(min, whole);
-    }
-
-    return min; // solve(C, R-1, carryOne(C, K));
+    else
+        return solve(C, R - 1, carryOne(C, K));
 }
